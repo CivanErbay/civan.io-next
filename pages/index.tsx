6fc5b5886d7e46f90513bf28af82243e0d8e4492
Civan <civan.erbay@web.de>
2022-08-25 20:33:06 +0200

    remove library

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,36 @@ import Head from "next/head";
 import Landing from "../components/Landing";
 import Footer from "../components/Footer";
 import Showcases from "../components/Showcases";
+import { useEffect, useState } from "react";
+import { useScrollDirection } from "react-use-scroll-direction";
 
 const Home: NextPage = () => {
+  const [offset, setOffset] = useState(0);
+
+  const { isScrollingUp, scrollDirection } = useScrollDirection();
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.pageYOffset);
+
+    // clean up code
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  console.log(scrollDirection);
+
   return (
     <div className="flex flex-col items-center justify-center max-w-6xl mx-auto lg:h-screen absolute inset-0 font-Trispace">
       <Head>
         <title>Civan</title>
         <meta name="description" content="Civan Erbay Web Development" />
         <link rel="icon" href="/favicon.ico" />
+        <div>{isScrollingUp && "Up"}</div>
       </Head>
       <Landing />
       <Footer />
-      <Showcases  />
+      <Showcases />
     </div>
   );
 };
