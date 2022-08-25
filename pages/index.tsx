153e4c49e431480272892379f33753aeeb3605cf
Civan <civan.erbay@web.de>
2022-08-25 20:52:13 +0200

    Add onWheel listener

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,32 +3,23 @@ import Head from "next/head";
 import Landing from "../components/Landing";
 import Footer from "../components/Footer";
 import Showcases from "../components/Showcases";
-import { useEffect, useState } from "react";
-import { useScrollDirection } from "react-use-scroll-direction";
 
 const Home: NextPage = () => {
-  const [offset, setOffset] = useState(0);
-
-  const { isScrollingUp, scrollDirection } = useScrollDirection();
-
-  useEffect(() => {
-    const onScroll = () => setOffset(window.pageYOffset);
-
-    // clean up code
-    window.removeEventListener("scroll", onScroll);
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-
-  console.log(scrollDirection);
+  //Finish page trigger logic
+  const triggerTransition = (e: any) => {
+    console.log(e.deltaY);
+    /*   e.deltaY > 0 ?  */
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center max-w-6xl mx-auto lg:h-screen absolute inset-0 font-Trispace">
+    <div
+      className="flex flex-col items-center justify-center max-w-6xl mx-auto lg:h-screen absolute inset-0 font-Trispace"
+      onWheel={(e) => triggerTransition(e)}
+    >
       <Head>
         <title>Civan</title>
         <meta name="description" content="Civan Erbay Web Development" />
         <link rel="icon" href="/favicon.ico" />
-        <div>{isScrollingUp && "Up"}</div>
       </Head>
       <Landing />
       <Footer />
