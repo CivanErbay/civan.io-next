3b7c1f641cc9dec3730040fa398f65ada0e2dfe0
Civan <civan.erbay@web.de>
2022-08-30 21:38:46 +0200

    Change impressum height

--- components/Impressum.tsx
+++ components/Impressum.tsx
@@ -5,14 +5,14 @@ export default function Footer() {
     <>
       <div className="bottom-0 flex justify-between items-end w-full fixed lg:px-5">
         <div
-          className={`relative flex items-center h-full bg-emerald-400 lg:bg-transparent ${style.impressum}`}
+          className={`relative flex items-center h-screen bg-emerald-400 lg:bg-transparent ${style.impressum}`}
         >
           <div className="h-[150px] w-[2px] bg-black mb-6"></div>
           <h2>civan.erbay@web.de &#x2f;&#x2f; 2022</h2>
         </div>
         <div className="relative flex flex-col lg:flex-row">
           <div
-            className={`flex items-center h-full bg-emerald-400 lg:bg-transparent ${style.impressum}`}
+            className={`flex items-center h-screen bg-emerald-400 lg:bg-transparent ${style.impressum}`}
           >
             <div className="h-[150px] w-[2px] bg-black mb-6"></div>
             <h2 className="mb-6">contact</h2>
