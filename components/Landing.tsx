45ccb6fb16560a8634edd66a5ce199126d18e0e7
Civan <Erbay.Civan.Extern@swm.de>
2022-10-23 17:02:22 +0200

    Add mobile styling

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -2,55 +2,83 @@
 import styles from "./Landing.module.css";
 import Image from "next/image";
 import { gsap } from "gsap";
-
+import { useEffect } from "react";
 
 export default function Landing() {
+  useEffect(() => {
+    gsap.fromTo(
+      ".headline-1",
+      { opacity: 0 },
+      { opacity: 1, duration: 1, delay: 2 }
+    );
+    gsap.fromTo(
+      ".headline-2",
+      { opacity: 0 },
+      { opacity: 1, duration: 1, delay: 5 }
+    );
+     gsap.fromTo(
+       ".headline-3",
+       { opacity: 0 },
+       { opacity: 1, duration: 1, delay: 6 }
+     );
+  }, []);
+
   return (
     <>
-      <div id="landingSection" className="relative min-h-[750px] md:h-[1000] w-full flex items-center justify-center flex-col md:flex-row">
+      <div
+        id="landingSection"
+        className="relative min-h-[750px] md:h-[1000] w-full flex items-center justify-center flex-col md:flex-row"
+      >
         <div
-          className={`top-[0px] left-0 w-[240px] h-auto md:w-[400px] md:h-auto md:top-[40px] md:left-[70px] absolute rounded-md tet-white  px-6 py-4 md:p-2 flex flex-col font-2xl opacity-0 ${styles.animate} `}
+          className={`top-[0px] left-0 w-[230px] h-auto md:w-[550px] md:h-auto md:top-[60px] md:left-[70px] absolute rounded-md tet-white  px-6 py-4 md:p-2 flex flex-col font-2xl md:text-7xl`}
         >
-          {/*  <div
-            className={`md:w-6 md:h-6 bg-white md:top-[15px] md:right-[15px] absolute rounded-md ${styles.pulseAnim} `}
-          ></div> */}
-          <span>Sup Visitor!</span>
-          <span className="text-4xl leading-none md:text-5xl py-1 font-bold">
-            I&apos;m <span className="font-bold ">Civan</span>
-          </span>
-          <span>welcome to my webspace</span>
-          {/*  <span>
+          <span className="headline-1">Sup Visitor!</span>
+          <br />
+          <span className="headline-2">welcome to my webspace</span>
+          {/*   <div className={styles.shiftedWrapper}>
+            <div className={`text-4xl leading-none md:text-7xl z-10 font-bold ${styles.shiftedContent}`}>
+              I&apos;m <span className="font-bold ">Civan</span>
+            </div>
+            <div className={styles.shiftedBottom}></div>
+            <div className={styles.shiftedLeft}></div>
+          </div>
+          */}
+          <div className={`headline-3 ${styles.button}`}>
+            <div className={styles.buttonLink}>
+              <h1>
+                I&apos;m <span className="font-bold text-left">Civan</span>
+              </h1>
+            </div>
+            <div className={styles.extrudeLeft}></div>
+            <div className={styles.extrudeLeftAlt}></div>
+            <div className={styles.extrudeBottom}></div>
+            <div className={styles.extrudeBottomAlt}></div>
+          </div>
+        </div>
+
+        {/*  <span>
               Every science is, among other things, an ordering, a simplifying,
               a digesting of the incomprehensible for the spirit. HH
             </span> */}
-        </div>
 
-        <div
+        {/* <div
           className={`w-[200px] h-[165px] top-[240px] right-18 md:w-[350px] md:h-[350px] md:right-[3px] md:top-[325px] absolute rounded-md md:rounded-md px-5 py-2 md:p-8 opacity-0 ${styles.animate2} ${styles.logo}`}
         >
-          {/*   <div
-            className={`md:w-6 md:h-6 bg-white md:top-[74px] md:left-[97px] absolute rounded-md ${styles.connector} `}
-          ></div>
-          <div
-            className={`md:w-6 md:h-6 bg-white md:top-[15px] md:left-[160px] absolute rounded-md ${styles.connector} `}
-          ></div> */}
           <Image
             className="civan"
             src="/pixel_me.svg"
-            /* layout="fill" */
+            /* layout="fill" 
             width={300}
             height={300}
             objectFit="contain"
             alt="civan erbay"
-          ></Image>
+          ></Image> 
+          
         </div>
 
         <div
           className={`bottom-[120px] w-[230px] right-16 md:bottom-auto md:top-[310px] md:left-[90px] md:w-[350px] absolute rounded-md text-white py-3 md:p-8 opacity-0 ${styles.animate3}`}
         >
-          {/*  <div
-            className={`md:w-7 md:h-7 bg-white md:bottom-[20px] md:left-[27px] absolute rounded-full`}
-          ></div> */}
           <h4 className="md:text-right font-2xl px-4">
             another passionate frontend developer
           </h4>
@@ -63,6 +91,7 @@ export default function Landing() {
             </a>
           </p>
         </div>
+        */}
       </div>
     </>
   );
